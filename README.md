20998ab38e22862c2952eaa641da205a60209fb2
谷永宾 <yonbin@163.com>
2021-01-09 21:14:01 +0800

    init

--- README.md
+++
@@ -1,1 +0,0 @@
-# yonbin.github.io
---
+++ common.js
@@ -0,0 +1,7 @@
+var appId = 'de77da2103acf7ecce955e722973f672';
+var apiKey = '462f9cd32b4b4667a30c133db861d467';
+var checkSessionUrl = 'https://api2.bmob.cn/1/checkSession/';
+var loginUrl = 'https://api2.bmob.cn/1/login';
+var submitUrl=' https://api2.bmob.cn/1/classes/data';
+
+$('#username').text($.cookie("username"));
