e9f246da50a2d047dcd5d1b8abd774c5933d2bec
谷永宾 <yonbin@163.com>
2021-01-17 09:25:11 +0800

    增加打字练习

---
+++ assets/js/common.js
@@ -0,0 +1,27 @@
+var appId = 'de77da2103acf7ecce955e722973f672';
+var apiKey = '462f9cd32b4b4667a30c133db861d467';
+var checkSessionUrl = 'https://api2.bmob.cn/1/checkSession/';
+var loginUrl = 'https://api2.bmob.cn/1/login';
+var submitUrl=' https://api2.bmob.cn/1/classes/data';
+
+$('#username').text($.cookie("username"));
+
+function getQueryVariable(variable) {
+    var query = window.location.search.substring(1);
+    var vars = query.split("&");
+    for (var i = 0; i < vars.length; i++) {
+        var pair = vars[i].split("=");
+        if (pair[0] == variable) { return pair[1]; }
+    }
+    return (false);
+}
+function getCurrentDate(date) {
+    var y = date.getFullYear();
+    var m = date.getMonth() + 1;
+    var d = date.getDate();
+    var h = date.getHours();
+    var min = date.getMinutes();
+    var s = date.getSeconds();
+    var str = y + '年' + (m < 10 ? ('0' + m) : m) + '月' + (d < 10 ? ('0' + d) : d) + '日  ' + (h < 10 ? ('0' + h) : h) + ':' + (min < 10 ? ('0' + min) : min) + ':' + (s < 10 ? ('0' + s) : s);
+    return str;
+}
---
+++ json/typingData.js
@@ -0,0 +1,10 @@
+var typingData=[
+    {
+        id:1,
+        data:"fjff fffff jj f fjj jff jfj"
+    },
+    {
+        id:2,
+        data:"fjff fffff jj f fjj jff jfj"
+    }
+]
