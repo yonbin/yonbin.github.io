4d3c533497a99d5998f809283e47196022da5ef4
谷永宾 <yonbin@163.com>
2021-01-23 21:04:27 +0800

    增加14题，增加指法练习游戏。

--- json/typingData.js
+++ json/typingData.js
@@ -1,27 +1,37 @@
 var typingData = [
     {
         id: 1,
-        title:'f和j',
+        title: 'f和j',
         data: "fjff fffff jj f fjj jff jfj jjjf fjjf ffjj jjff jjjjj fff jj"
     },
     {
         id: 2,
-        title:'d和k',
+        title: 'd和k',
         data: "dkdd dddd kk dd kkkk dkdk kdkd kkkk"
     },
     {
         id: 3,
-        title:'s和l',
+        title: 's和l',
         data: "sssl llls slsl lsls llll ssss lslss sssl llss"
     },
     {
         id: 4,
-        title:'a和;',
+        title: 'a和;',
         data: "a;;a ;;;; aaaa a;a; ;a;a ;;;a aaa; aa;;"
     },
     {
         id: 5,
-        title:'g和h',
+        title: 'g和h',
         data: "gggh ghgh hghg hhhh gghh ghhg hhg hhgh"
+    },
+    {
+        id: 6,
+        title: '练习1',
+        data: "ask dad all salads fall; lass as all ads add salsa jaff kad; flask lass sad fall alfa jak kaj dad ask"
+    },
+    {
+        id: 7,
+        title: '练习2',
+        data: "askdadallsaladsfall;lassasalladsaddsalsajaffkad;flasklasssadfallalfajakkajdadask"
     }
 ]
